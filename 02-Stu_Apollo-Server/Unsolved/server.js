ed21c174222c5b560fa432101b2033d3ff98c662
farhahdin1997 <farhahdin97@hotmail.com>
2022-05-17 19:27:19 +0100

    taask

---
+++ 02-Stu_Apollo-Server/Unsolved/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+// TODO: Add a comment describing the functionality of this expression
+// requiring appolo package
+const { ApolloServer } = require('apollo-server-express');
+
+// TODO: Add a comment describing the functionality of this expression
+//bringing in the schema and connection
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// TODO: Add a comment describing the functionality of this async function
+// Create a new instance of an Apollo server with the GraphQL schema
+
+const startApolloServer = async (typeDefs, resolvers) => {
+await server.start();
+server.applyMiddleware({ app });
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  })
+})
+};
+
+// TODO: Add a comment describing this functionality
+//starting the server
+startApolloServer(typeDefs, resolvers);
